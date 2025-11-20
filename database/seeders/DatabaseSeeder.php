66a801d10954f62c9111367bba1a93355cea7a26
Doctor3131 <sirielwnf@gmail.com>
2025-11-20 13:30:37 +0700

    update the authentication for using the db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,31 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        // Test User Default - Disable 2FA
+        User::factory()->withoutTwoFactor()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        // User Taqi - Disable 2FA
+        User::factory()->withoutTwoFactor()->create([
+            'name' => 'Taqi',
+            'email' => 'taqi@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        // User Hidah - Disable 2FA
+        User::factory()->withoutTwoFactor()->create([
+            'name' => 'Hidah',
+            'email' => 'hidah@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        // User Raya - Disable 2FA
+        User::factory()->withoutTwoFactor()->create([
+            'name' => 'Raya',
+            'email' => 'raya@gmail.com',
+            'password' => Hash::make('12345678'),
         ]);
     }
 }
